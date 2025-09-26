44f967765830e490d93ed6d936bf06b01f9354f3
uwera-creator <uwerajox@gmail.com>
2025-09-26 17:10:50 +0200

    Add files via upload

---
+++ LOAN MANAGEMENT SYSTEM_26_09_2025/src/panel/Customerpanel.java
@@ -0,0 +1,16 @@
+package panel;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JPanel;
+
+public class Customerpanel extends JPanel implements ActionListener{
+
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
